86df253cf01e7cbc56a4f9a23abe7d7218ef4b61
Jedrzej Lewandowski <progreacja@gmail.com>
2018-09-17 22:49:36 +0200

    Update File Upload

--- src/app/cms/components/common/input-file/input-file.component.ts
+++ src/app/cms/components/common/input-file/input-file.component.ts
@@ -4,21 +4,17 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core'
   selector: 'app-input-file',
   template: `
     <div class="file">
-      <input id="fileInput"
-             type="file"
-             (change)="changed.emit($event)"
-             multiple>
-      <label for="fileInput">
-        <i class="material-icons">cloud_upload</i>
-        <span>or choose a file…</span>
-      </label>
+      <input id="fileInput" type="file" (change)="changed.emit($event)" #photoInput multiple>
+      <button mat-raised-button color="warn" (click)="photoInput.click()">
+         <i class="material-icons">cloud_upload</i>
+         <span>Select Files</span>
+      </button>
     </div>
   `,
   styleUrls: ['./input-file.component.scss'],
 })
 export class InputFileComponent implements OnInit {
-  @Output()
-  changed = new EventEmitter()
+  @Output() changed = new EventEmitter()
 
   constructor() {}
 
