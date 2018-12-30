bb12c690de6dfc0ec2a72b34a39a891fe441ee8f
Jedrzej Lewandowski <progreacja@gmail.com>
2018-12-30 16:38:38 +0100

    Folder Updates

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core'
-import { HomeComponent } from './home/home.component'
-import { GridComponent } from './grid/grid.component'
+import { HomeComponent } from './components/home/home.component'
+import { GridComponent } from './components/grid/grid.component'
 import { HeaderComponent } from './common/header/header.component'
-import { GalleryComponent } from './gallery.component'
+import { GalleryComponent } from './components/gallery-root/gallery.component'
 import { GalleryRoutingModule } from './gallery-routing.module'
 
 @NgModule({
