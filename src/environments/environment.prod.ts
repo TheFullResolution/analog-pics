26f29379b3007701f0923a135d6f9a0db6d38f0f
Jedrzej <progreacja@gmail.com>
2019-07-25 22:45:39 +0200

    Whatever

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
-    production: true,
-  };
-  
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBLk2bdC3waMeuHGM6AuUJRCKV5LPwpbww',
+    authDomain: 'analog-pics-a1a3b.firebaseapp.com',
+    databaseURL: 'https://analog-pics-a1a3b.firebaseio.com',
+    projectId: 'analog-pics-a1a3b',
+    storageBucket: 'analog-pics-a1a3b.appspot.com',
+    messagingSenderId: '851287805520',
+  },
+};
